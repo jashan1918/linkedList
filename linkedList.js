9c6f5b7632a68c42f28601b014acaf9457c2e901
Jashan <jashandeep4329@gmail.com>
2025-10-05 17:26:12 +0530

    implement linked list with append method

--- linkedList.js
+++ linkedList.js
@@ -1,1 +1,40 @@
-console.log("start");
+class Node {
+
+    constructor(value) {
+
+        this.value = value;
+        this.nextNode = null;
+    }
+}
+
+class LinkedList {
+
+    constructor() {
+  
+        this.head = null;
+        this.tail = null;
+ }
+
+    append(value) {
+        //create a new node
+        let newNode = new Node(value);
+
+        //check if the list is empty
+
+        if(this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            //add new node after current tail
+            this.tail.nextNode = newNode;
+
+            //move tail to the new next Node
+            this.tail = newNode;
+        }
+    }
+}
+
+const list = new LinkedList();
+
+
